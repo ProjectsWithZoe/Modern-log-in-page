a961e796c3de03b8d1b0110c565cee15fb10032f
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2024-02-07 16:35:04 +0000

    Log in page changes colors

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
-const button = document.querySelectorAll("button");
-const colorContainer = document.getElementsByClassName("color-container");
+const signinBtn = document.getElementById("signin");
+const buttons = document.querySelectorAll("button");
+const redColor = document.getElementById("red-color");
+const blueColor = document.getElementById("blue-color");
+const greenColor = document.getElementById("green-color");
+const orangeColor = document.getElementById("orange-color");
 
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
@@ -12,6 +16,43 @@ loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
-colorContainer.addEventListener("click", () => {
-  console.log("clicked");
+function changeButtonColors(color) {
+  buttons.forEach((button) => {
+    button.style.backgroundColor = color;
+  });
+}
+function changeToggleColor(color) {
+  const toggleElements = document.querySelectorAll(".toggle");
+
+  toggleElements.forEach((toggleElement) => {
+    toggleElement.style.background = `linear-gradient(to right, grey, ${color}`;
+  });
+}
+
+redColor.addEventListener("click", (event) => {
+  const computedStyle = window.getComputedStyle(event.target);
+  const color = computedStyle.backgroundColor;
+  changeButtonColors(color);
+  changeToggleColor(color);
+});
+
+blueColor.addEventListener("click", (event) => {
+  const computedStyle = window.getComputedStyle(event.target);
+  const color = computedStyle.backgroundColor;
+  changeButtonColors(color);
+  changeToggleColor(color);
+});
+
+greenColor.addEventListener("click", (event) => {
+  const computedStyle = window.getComputedStyle(event.target);
+  const color = computedStyle.backgroundColor;
+  changeButtonColors(color);
+  changeToggleColor(color);
+});
+
+orangeColor.addEventListener("click", (event) => {
+  const computedStyle = window.getComputedStyle(event.target);
+  const color = computedStyle.backgroundColor;
+  changeButtonColors(color);
+  changeToggleColor(color);
 });
